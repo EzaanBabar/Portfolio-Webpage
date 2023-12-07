08dcc883b8f89b58d269d9874f194ecc7b2f1a4d
Ezaan Babar <ezaan0801@gmail.com>
2023-12-06 22:34:09 -0600

    Worked on making the webpage responsive for different resolutions, including tablet and mobile. Contact page has been introduced as well

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function toggleDarkMode() {
-    document.querySelector('header').classList.toggle('dark-mode');
-    document.querySelector('h1').classList.toggle('dark-mode');
+let is_contact_open = false;
+function toggle_contact() {
+  is_contact_open = !is_contact_open; // Toggle the state
+  if (is_contact_open) {
+    document.body.classList.add("contact--open");
+  } else {
+    document.body.classList.remove("contact--open");
   }
+}
