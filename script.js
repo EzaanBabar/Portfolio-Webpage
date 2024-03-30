b2f7ade2c1a072a3caa6b5ef9f82aa53fab6269f
Ezaan  Babar <ezaan0801@gmail.com>
2024-03-30 04:15:28 -0500

    Updated projects with new links for HTTPS

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ function handleWrapperClick() {
   // Add your logic here to handle the click event
   // For example, navigate to another webpage
   //window.location.href = 'http://jfauto.s3-website.us-east-2.amazonaws.com/';
-  window.open('http://gmw-website.s3-website.us-east-2.amazonaws.com/', '_blank');
+  window.open('https://gmw-webpage.vercel.app/', '_blank');
 
 }
 function handleWrapperClick2() {
   // Add your logic here to handle the click event
   // For example, navigate to another webpage
-  window.open('https://d2e3f6wac1sypc.cloudfront.net/', '_blank');
+  window.open('https://jc-autodetail.com/', '_blank');
 }
